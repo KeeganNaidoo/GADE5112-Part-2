442b8e1ca7856e30cc7781ba5c9a88d0f307e332
Faraaz Suffla <faraazsuffla29@gmail.com>
2022-11-18 19:12:54 +0200

    Leader Class added

---
+++ Basic Hero Game/Leader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Hero_Game
+{
+    public class Leader : Enemy
+    {
+        private Tile target { get; set; }
+
+        public Leader(int x, int y, int enemyArrayNum) : base(x, y, enemyArrayNum)
+        {
+            HP = 20;
+            Damage = 2;
+        }
+        public override Movement ReturnMove(Movement move = 0)
+        {
+
+        }
+    }
+}
